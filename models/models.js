0cc68162174ab9f2acfa4ab9dde582cee4720d8d
Jorge Sánchez <jorsanqu@hotmail.com>
2015-07-17 18:36:57 +0200

    --Despliegue DB en Heroku 3

--- models/models.js
+++ models/models.js
@@ -33,7 +33,7 @@ exports.Quiz = Quiz; // exportar definición de tabla Quiz
 
 // sequelize.sync() crea e inicializa tabla de preguntas en DB
 
-sequelize.sync().success(function (count) {
+sequelize.sync().success(function () {
    // success(..) ejecuta el manejador una vez creada la tabla
    Quiz.count().success(function (count){
       if (count === 0) {  // la tabla se inicializa solo si está vacía
