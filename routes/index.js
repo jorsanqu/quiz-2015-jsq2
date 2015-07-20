2e404d374aef3e65af7f25c1f6a38f4fe17112da
Jorge Sánchez <jorsanqu@hotmail.com>
2015-07-20 18:02:56 +0200

    M8 Añadir Preguntas

--- routes/index.js
+++ routes/index.js
@@ -11,9 +11,13 @@ router.get('/', function(req, res) {
 // Autoload de comandos con :quizId
 router.param('quizId', quizController.load); // autoload : quizId
 
+// Definición de rutas de /quizes
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
+
 router.get('/author', quizController.author);
 
 module.exports = router;
